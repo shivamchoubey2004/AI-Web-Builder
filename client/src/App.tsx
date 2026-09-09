02621f37df30a27b865c8b78b5a60166650fbf5e
shivamchoubey2004 <shivamchoubey2004@gmail.com>
2026-09-09 20:07:24 +0530

    Initial React project (Folder structure) completed

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProjects from './pages/MyProjects'
+import Preview from './pages/Preview'
+import Community from './pages/Community'
+import View from './pages/View'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
